9add9e30b6469afa59c9ad2f0501c387facdfb50
RensPitman <renskuilman@gmail.com>
2019-03-26 15:08:45 +0100

    Main Menu

---
+++ WorkedUp/Assets/Scripts/UI/MainMenu/MainMenuControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuControl : MonoBehaviour
+{
+
+    public CanvasGroup MainMenu;
+    public CanvasGroup InstructionsMenu;
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void OpenMainMenu()
+    {
+        MainMenu.alpha = 1;
+        MainMenu.interactable = true;
+        MainMenu.blocksRaycasts = true;
+
+        InstructionsMenu.alpha = 0;
+        InstructionsMenu.interactable = false;
+        InstructionsMenu.blocksRaycasts = false;
+    }
+
+    public void OpenHowToPlay()
+    {
+        MainMenu.alpha = 0;
+        MainMenu.interactable = false;
+        MainMenu.blocksRaycasts = false;
+
+        InstructionsMenu.alpha = 1;
+        InstructionsMenu.interactable = true;
+        InstructionsMenu.blocksRaycasts = true;
+    }
+}
